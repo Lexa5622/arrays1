280722d3ab715c5d3842f7b7da797ba2801a6fba
Алексей Дорофеев <aleksey.dorofeev@rmg-region.ru>
2024-12-10 21:35:36 +0300

    start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
                 arrInt[0] = 1;
                 arrInt[1] = 2;
                 arrInt[2] = 3;
+
         double [] arrDouble = {1.57, 7.654, 9.986};
         String[] myArray = {"a", "b", "c", "d", "e"};
         System.out.println("Задание 2");
@@ -23,7 +24,7 @@ public class Main {
         }
 
         for (int i = 0; i < arrDouble.length; i ++){
-            if (i == arrInt.length - 1){
+            if (i == arrDouble.length - 1){
                 System.out.println(arrDouble[i]);
                 break;
             }
@@ -31,7 +32,7 @@ public class Main {
         }
 
         for (int i = 0; i < myArray.length; i++){
-            if (i == arrInt.length - 1){
+            if (i == myArray.length - 1){
                 System.out.println(myArray[i]);
                 break;
             }
@@ -41,7 +42,7 @@ public class Main {
         System.out.println("Задание 3");
 
         for (int i = arrInt.length -1; i  >= 0; i --){
-            if (i  == 0){
+            if (i == 0){
                 System.out.println(arrInt[i]);
                 break;
             }
@@ -67,7 +68,7 @@ public class Main {
         System.out.println("Задание 4");
 
         for (int i = 0; i  < arrInt.length; i ++){
-            if (i  % 2 == 0){
+            if (arrInt[i]  % 2 != 0){
                 arrInt[i] += 1;
                 if (i == arrInt.length - 1){
                     System.out.println(arrInt[i]);
